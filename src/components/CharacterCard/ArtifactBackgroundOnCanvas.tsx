dce8b34597bfae487322725c426ae5351abf9f7a
Kamil Masłowiec <kpmaslowiec@gmail.com>
2023-12-30 03:44:37 +0100

    fix for iOS - replace filter:brightness with a mask

--- src/components/CharacterCard/ArtifactBackgroundOnCanvas.tsx
+++ src/components/CharacterCard/ArtifactBackgroundOnCanvas.tsx
@@ -38,10 +38,9 @@ export const ArtifactBackgroundOnCanvas: React.FC<{
       ctx!.globalCompositeOperation = "source-out";
       ctx!.clearRect(0, 0, canvasWidth, canvasHeight);
 
-      // doesnt work?
-      ctx!.filter = `blur(${namecardBg ? 1 : 0}px) brightness(${
-        adaptiveBgColor ? 50 : 65
-      }%)`;
+      // doesnt work on safari iOS?
+      // ctx!.filter = `brightness(${adaptiveBgColor ? 50 : 65}%)`;
+      ctx!.filter = `blur(${namecardBg ? 1 : 0}px)`;
 
       ctx!.scale(scaleFactor, scaleFactor);
       ctx!.globalCompositeOperation = "source-over";
@@ -56,32 +55,41 @@ export const ArtifactBackgroundOnCanvas: React.FC<{
         (namecardBg ? 280 : img.height) * conditionalScale
       );
 
-      if (!adaptiveBgColor || !adaptiveColors) return;
-
       const gradientCoords = [0, 0, 0, canvasHeight] as const;
-      const adaptiveGradientSolid = ctx!.createLinearGradient(
-        ...gradientCoords
-      );
-      const adaptiveGradient = ctx!.createLinearGradient(...gradientCoords);
+      
+      if (adaptiveBgColor && adaptiveColors) {
+        const adaptiveGradientSolid = ctx!.createLinearGradient(
+          ...gradientCoords
+        );
+        const adaptiveGradient = ctx!.createLinearGradient(...gradientCoords);
+  
+        const fillColorSolid = adaptiveColors[0];
+        adaptiveGradientSolid.addColorStop(0, fillColorSolid[0]);
+        adaptiveGradientSolid.addColorStop(
+          0.5,
+          fillColorSolid[fillColorSolid.length - 2]
+        );
+  
+        const fillColor = adaptiveColors[1];
+        adaptiveGradient.addColorStop(0, fillColor[0]);
+        adaptiveGradient.addColorStop(0.5, fillColor[fillColor.length - 2]);
+  
+        ctx!.globalCompositeOperation = "color";
+        ctx!.fillStyle = adaptiveGradientSolid;
+        ctx!.fillRect(0, 0, canvasWidth, canvasHeight);
+  
+        ctx!.globalCompositeOperation = "hard-light";
+        ctx!.fillStyle = adaptiveGradient;
+        ctx!.filter = "contrast(150%)"; // 1.2 ? 120% ?
+        ctx!.fillRect(0, 0, canvasWidth, canvasHeight);
+      }
 
-      const fillColorSolid = adaptiveColors[0];
-      adaptiveGradientSolid.addColorStop(0, fillColorSolid[0]);
-      adaptiveGradientSolid.addColorStop(
-        0.5,
-        fillColorSolid[fillColorSolid.length - 2]
-      );
+      // brightness hack for safari iOS
+      const brightnessMask = ctx!.createLinearGradient(...gradientCoords);
+      brightnessMask.addColorStop(0, `#000000${adaptiveBgColor ? '75' : '58'}`);
 
-      const fillColor = adaptiveColors[1];
-      adaptiveGradient.addColorStop(0, fillColor[0]);
-      adaptiveGradient.addColorStop(0.5, fillColor[fillColor.length - 2]);
-
-      ctx!.globalCompositeOperation = "color";
-      ctx!.fillStyle = adaptiveGradientSolid;
-      ctx!.fillRect(0, 0, canvasWidth, canvasHeight);
-
-      ctx!.globalCompositeOperation = "hard-light";
-      ctx!.fillStyle = adaptiveGradient;
-      ctx!.filter = "contrast(1.2)";
+      ctx!.globalCompositeOperation = "source-over";
+      ctx!.fillStyle = brightnessMask;
       ctx!.fillRect(0, 0, canvasWidth, canvasHeight);
     };
   }, [canvasRef, adaptiveBgColor, namecardBg, backgroundImage, adaptiveColors]);
