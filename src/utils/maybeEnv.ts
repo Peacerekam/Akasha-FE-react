dfab4349d1b70c77db8fae7aa33fc7704c5f48c2
Kamil Masłowiec <kpmaslowiec@gmail.com>
2024-04-29 21:53:35 +0200

    prod

--- src/utils/maybeEnv.ts
+++ src/utils/maybeEnv.ts
@@ -4,5 +4,5 @@ export const MAINTENANCE_MODE = false;
 export const SHOW_ADS = true;
 
 // probably change
-export const IS_PRODUCATION = false; // set false to use localhost endpoints
+export const IS_PRODUCATION = true; // set false to use localhost endpoints
 export const TRANSLATION_VERSION = 0.286; // increment this when translation keys are outdated
